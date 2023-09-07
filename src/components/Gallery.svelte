31012f133405067e88a0efd56c75f22ae33f813b
Lewin Kelly <heli@odex.cf>
2023-09-07 03:01:59 +0100

    Update font and replace sass styling with stylus

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -32,70 +32,71 @@
 	</div>
 {/if}
 
-<style lang="sass">
+<style lang="stylus">
 	#all
-		max-width: 100%
-		display: grid
-		column-gap: 0.5rem
-		row-gap: 0.5rem
-		grid-template-columns: repeat(4, 1fr)
+		max-width 100%
+		display grid
+		column-gap 0.5rem
+		row-gap 0.5rem
+		grid-template-columns repeat(4, 1fr)
 
-		@media screen and (max-width: 800px)
-			grid-template-columns: repeat(3, 1fr)
-		@media screen and (max-width: 500px)
-			grid-template-columns: repeat(2, 1fr)
+		@media screen and (max-width 800px)
+			grid-template-columns repeat(3, 1fr)
+		@media screen and (max-width 500px)
+			grid-template-columns repeat(2, 1fr)
 
 	#bg
-		position: fixed
-		top: 0
-		left: 0
-		width: 100vw
-		height: 100vh
-		background: #000e
-		z-index: 999
+		position fixed
+		top 0
+		left 0
+		width 100vw
+		height 100vh
+		background #000e
+		z-index 999
 
 		img
-			width: 100%
-			height: calc(100% - 18rem)
-			position: fixed
-			top: 50%
-			left: 50%
-			transform: translate(-50%, -48%)
+			width 100%
+			height calc(100% - 18rem)
+			position fixed
+			top 50%
+			left 50%
+			transform translate(-50%, -48%)
 
 	button
-		background: #0000
-		border: 0
-		padding: 0
+		background #0000
+		border 0
+		padding 0
 
-	button, img
-		width: 100%
-		height: 100%
-		object-fit: contain
-		border-radius: 1rem
+	button
+	img
+		width 100%
+		height 100%
+		object-fit contain
+		border-radius 1rem
 
 	.galleryimg
-		cursor: pointer
-		transition: 0.2s
+		cursor pointer
+		transition 0.2s
 		&:hover
-			transition: 0.2s
-			transform: scale(1.2)
+			transition 0.2s
+			transform scale(1.2)
 
-	h1, h2
-		text-align: center
-		margin-top: 6rem
+	h1
 	h2
-		position: fixed
-		bottom: 0
-		left: 50%
-		transform: translateX(-50%)
-		height: 6rem
-		@media screen and (max-width: 800px)
-			width: 60%
+		text-align center
+		margin-top 6rem
+	h2
+		position fixed
+		bottom 0
+		left 50%
+		transform translateX(-50%)
+		height 6rem
+		@media screen and (max-width 800px)
+			width 60%
 
-
-	@media screen and (max-width: 600px)
+	@media screen and (max-width 600px)
 		h1
-			margin-top: 3rem
+			margin-top 3rem
 		h2
-			width: 85%
+			width 85%
 </style>
