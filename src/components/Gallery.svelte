7817aa81dbe6da05ee41014c6074ca83b1829a6c
Lewin Kelly <heli@odex.cf>
2025-12-06 22:37:59 +0000

    Replace Stylus with standard CSS

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -23,7 +23,10 @@
 <br />
 <div id="all">
 	{#each json as item}
-		<button class="galleryimg" on:click={open(item)} on:keypress={open(item)}>
+		<button
+			class="galleryimg"
+			on:click={open(item)}
+			on:keypress={open(item)}>
 			<img
 				src="https://cdn.torbreckorchard.co.uk/gallery/{item.Filename}.webp"
 				alt={item.Name} />
@@ -47,77 +50,83 @@
 	</div>
 {/if}
 
-<style lang="stylus">
-	#all
-		max-width 100%
-		display grid
-		column-gap 0.5rem
-		row-gap 0.5rem
-		grid-template-columns repeat(4, 1fr)
-
-		@media screen and (max-width 800px)
-			grid-template-columns repeat(3, 1fr)
-		@media screen and (max-width 500px)
-			grid-template-columns repeat(2, 1fr)
-
-	#bg
-		position fixed
-		top 0
-		left 0
-		width 100vw
-		height 100vh
-		background #000e
-		z-index 999
-
-		img
-			width 100%
-			height calc(100% - 18rem)
-			position fixed
-			top 50%
-			left 50%
-			transform translate(-50%, -48%)
-
-	button
-		background #0000
-		border 0
-		padding 0
-
-	button
-	img
-		width 100%
-		height 100%
-		object-fit contain
-		border-radius 1rem
-
-	.galleryimg
-		cursor pointer
-		transition 0.2s
-		&:hover
-			transition 0.2s
-			transform scale(1.2)
-
-	h2
-	h3
-		text-align center
-		margin-top 6rem
-
-	h2
-		font-size 2.5rem
-
-	h3
-		font-size 1.8rem
-		position fixed
-		bottom 0
-		left 50%
-		transform translateX(-50%)
-		height 6rem
-		font-weight 400
-		@media screen and (max-width 800px)
-			width 60%
-
-	@media screen and (max-width 600px)
-		h2
-			margin-top 3rem
-		h3
-			width 85%
+<style>
+	#all {
+		max-width: 100%;
+		display: grid;
+		column-gap: 0.5rem;
+		row-gap: 0.5rem;
+		grid-template-columns: repeat(4, 1fr);
+		@media screen and (max-width: 800px) {
+			grid-template-columns: repeat(3, 1fr);
+		}
+		@media screen and (max-width: 500px) {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+	#bg {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		background: rgba(0, 0, 0, 0.933);
+		z-index: 999;
+		img {
+			width: 100%;
+			height: calc(100% - 18rem);
+			position: fixed;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -48%);
+		}
+	}
+	button {
+		background: rgba(0, 0, 0, 0);
+		border: 0;
+		padding: 0;
+	}
+	button,
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+		border-radius: 1rem;
+	}
+	.galleryimg {
+		cursor: pointer;
+		transition: 0.2s;
+		&:hover {
+			transition: 0.2s;
+			transform: scale(1.2);
+		}
+	}
+	h2,
+	h3 {
+		text-align: center;
+		margin-top: 6rem;
+	}
+	h2 {
+		font-size: 2.5rem;
+	}
+	h3 {
+		font-size: 1.8rem;
+		position: fixed;
+		bottom: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		height: 6rem;
+		font-weight: 400;
+		@media screen and (max-width: 800px) {
+			width: 60%;
+		}
+	}
+	@media screen and (max-width: 600px) {
+		h2 {
+			margin-top: 3rem;
+		}
+		h3 {
+			width: 85%;
+		}
+	}
 </style>
